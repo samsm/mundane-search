0ce1ca75fea242ece081097f20f50dcfcde1462f
Sam Schenkman-Moore <samsm@samsm.com>
2012-12-08 15:04:10 -0500

    Basic filter.

--- lib/mundane-search/filter.rb
+++ lib/mundane-search/filter.rb
@@ -1,31 +1,66 @@
+require 'active_support/hash_with_indifferent_access'
 module MundaneSearch
+  class InvalidOption < StandardError ; end
   class Filter
     # {
     #   name: 'Awesomefilter',
     #   target: 'created_at',
-    #   search_type: 'time',
+    #   search_type: 'time', # optional
+    #   search_method: ->(scope, term) { scope.where(["foo = ? ", term]) }
+    #   search_procs: {active_record: ->(term) {...} }
+    #   filter_mechanism: 'ActiveRecord', # Sunspot, etc.
+    #   engine: 'Postgres' # figure out a better term here
     #   optional: true,
     #   validate_with: ->(val) { val.kind_of?(Time) }, # or /\A\S{4,}\Z/
     #   validations: [],
     #   error_on_validate: false
     # }
-    # %w(target search_type optional validate_with validations error_on_validation_failure)
+    def self.valid_options
+      %w(target search_type search_method optional validate_with validations
+         error_on_validation_failure)
+    end
+
+    def self.valid_option?(key)
+      unless valid_options.include?(key)
+        raise InvalidOption
+      end
+      true
+    end
+
     attr_accessor :options
     def initialize(options = {})
-      self.options = HashWithIndifferentAccess.new.merge(options)
+      options = HashWithIndifferentAccess.new.merge(options)
+      self.options = options.reject {|k,v| !self.class.valid_option?(k) }
     end
 
     def valid? # is the filter ready to accept scope + params?
       # do we know what kind of search?
       # does it have a name?
       # etc ...
+      true
     end
 
-    def valid_scope?(scope)   ; end
-    def valid_params?(params) ; end
+    # The field(s) to search
+    def target
+      options[:target]
+    end
+
+    # is the scope valid? AR scope vs. array, etc
+    def valid_scope?(scope)
+      true
+    end
+
+    # Are the given params valid? If an attribute is required, is it present?
+    def valid_params?(params)
+      true
+    end
+
+    def search_method
+      options[:search_method]
+    end
 
     def apply_to(scope, params = {})
-      scope
+      search_method.call(scope, params)
     end
   end
 end
--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -5,4 +5,17 @@ describe MundaneSearch::Filter do
   it "should be a class" do
     Filter.must_be_kind_of Class
   end
+
+  it "should reject bad options" do
+    proc {
+      Filter.new(invalid_option: 1)
+    }.must_raise(MundaneSearch::InvalidOption)
+  end
+
+  it "should filter with proc" do
+    proc = ->(scope, params) { scope.select {|s| s > 2 } }
+    filter = Filter.new( search_method: proc )
+    filter.apply_to([1,2,3,4,5]).must_equal([3,4,5])
+  end
+
 end

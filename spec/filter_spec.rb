681bef59ba7e346ec11754ee1bd0833c6e28bbf2
Sam Schenkman-Moore <samsm@samsm.com>
2012-12-14 15:10:36 -0500

    Removed super-basic test.

--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -2,9 +2,6 @@ require_relative 'minitest_helper'
 
 describe MundaneSearch::Filter do
   Filter = MundaneSearch::Filter
-  it "should be a class" do
-    Filter.must_be_kind_of Class
-  end
 
   it "should reject bad options" do
     proc {

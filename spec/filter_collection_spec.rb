ae220d161394795e1b64b266dcc0eb8574b00810
Sam Schenkman-Moore <samsm@samsm.com>
2012-12-14 16:10:31 -0500

    Use multiple filters.

--- spec/filter_collection_spec.rb
+++ spec/filter_collection_spec.rb
@@ -3,16 +3,25 @@ require_relative 'minitest_helper'
 describe MundaneSearch::FilterCollection do
   FilterCollection = MundaneSearch::FilterCollection
 
-  let(:filter) {
-    MiniTest::Mock.new.expect(:apply_to, [1,2], [collection,params]) }
+  let(:filter) { mock_filter([1,2], [collection,params]) }
   let(:params) { Hash.new }
   let(:filter_method) { ->(c,p) { c.pop ; c } }
   let(:collection)    { [1,2,3] }
   let(:params) { Hash.new }
 
-  it "should use dummy filter" do
+  def mock_filter(returns, args)
+    MiniTest::Mock.new.expect(:apply_to, returns, args)
+  end
+
+  it "should use filter" do
     fc = FilterCollection.new([filter])
-    fc.execute(collection, params)
+    fc.execute(collection, params).must_equal [1,2]
+  end
+
+  it "should use multiple filters" do
+    filter2 = mock_filter([1], [[1,2], params])
+    fc = FilterCollection.new([filter, filter2])
+    fc.execute(collection, params).must_equal [1]
   end
 
 end
